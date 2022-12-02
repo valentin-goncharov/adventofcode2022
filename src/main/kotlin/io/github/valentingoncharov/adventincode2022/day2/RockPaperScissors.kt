25720b556712c9d00b7f8a13e8209c8a3dcd11a8
Valentin Goncharov <goncharov.valentin@gmail.com>
2022-12-02 12:58:56 +0100

    #1 - day 2 part 1

---
+++ src/main/kotlin/io/github/valentingoncharov/adventincode2022/day2/RockPaperScissors.kt
@@ -0,0 +1,42 @@
+package io.github.valentingoncharov.adventincode2022.day2
+
+import java.io.File
+
+/*
+
+A, X - Rock (1)
+B, Y - Paper (2)
+C, Z - Scissors (3)
+
+Loss - 0
+Draw - 3
+Win - 6
+
+- Rock defeats Scissors
+- Scissors defeats Paper
+- Paper defeats Rock
+
+ */
+fun main() {
+    val combinations = mapOf(
+        "A X" to 4, // rock - rock, draw
+        "A Y" to 8, // rock - paper, win
+        "A Z" to 3, // rock - scissors, loss
+
+        "B X" to 1, // paper - rock, loss
+        "B Y" to 5, // paper - paper, draw
+        "B Z" to 9, // paper - scissors, win
+
+        "C X" to 7, // scissors - rock, win
+        "C Y" to 2, // scissors - paper, loss
+        "C Z" to 6, // scissors - scissors, draw
+    )
+
+    val scores = File("/Users/valentingoncharov/Work/adventofcode2022/src/main/resources/rock_paper_scissors.txt")
+        .readLines()
+        .map{ combinations[it] }
+        .filterNotNull()
+        .sum()
+
+    println(scores)
+}
