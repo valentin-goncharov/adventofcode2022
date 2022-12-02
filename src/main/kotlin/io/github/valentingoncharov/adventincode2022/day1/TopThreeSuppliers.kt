24f065055e9ff957de2f984e59b09c41a80786f0
Valentin Goncharov <goncharov.valentin@gmail.com>
2022-12-02 12:38:07 +0100

    #1 - day 1 part 2

---
+++ src/main/kotlin/io/github/valentingoncharov/adventincode2022/day1/TopThreeSuppliers.kt
@@ -0,0 +1,27 @@
+package io.github.valentingoncharov.adventincode2022.day1
+
+import java.io.File
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(File("/Users/valentingoncharov/Work/adventofcode2022/src/main/resources/elf_supplies.txt"))
+    val supplies = mutableListOf<Int>()
+    var cur = 0
+
+    while(scanner.hasNextLine()) {
+        val line = scanner.nextLine()
+        if (line.isNotBlank()) {
+            cur += line.toInt()
+        } else {
+            supplies.add(cur)
+            cur = 0
+        }
+    }
+
+    if (cur > 0) {
+        supplies.add(cur)
+    }
+
+    supplies.sortDescending()
+    println(supplies.take(3).sum())
+}
