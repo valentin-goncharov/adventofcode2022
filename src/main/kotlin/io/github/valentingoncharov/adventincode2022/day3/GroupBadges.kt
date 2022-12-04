b0200ee42f6a6f6354b6e2e48bacda9b159dd475
Valentin Goncharov <goncharov.valentin@gmail.com>
2022-12-04 10:53:56 +0100

    #1 - day 3 part 2

---
+++ src/main/kotlin/io/github/valentingoncharov/adventincode2022/day3/GroupBadges.kt
@@ -0,0 +1,27 @@
+package io.github.valentingoncharov.adventincode2022.day3
+
+import java.io.File
+
+fun main() {
+
+    val priorities = (('a'..'z').mapIndexed { index, c -> c to index + 1 } +
+            ('A'..'Z').mapIndexed { index, c -> c to index + 27 }).toMap()
+
+    val scores = File("/Users/valentingoncharov/Work/adventofcode2022/src/main/resources/rucksacks.txt")
+        .readLines()
+        .chunked(3)
+        .withIndex()
+        .flatMap{
+            val first = it.value[0]
+            val second = it.value[1]
+            val third = it.value[2]
+
+            val diff = first.asIterable().toSet() intersect  second.asIterable().toSet() intersect third.asIterable().toSet()
+            println("${it.index}: $diff")
+            diff.map { char -> priorities[char] }
+        }
+        .filterNotNull()
+        .sum()
+
+    println(scores)
+}
